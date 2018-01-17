4cb097681ba204f2ef46d374bc9dd75fa65d4ec0
Benoit PASQUIER <b.pasquier69@gmail.com>
2018-01-17 22:06:12 +0000

    Adding AlbumDetail + Card + CardSection

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    return (
+        <Card>
+            <CardSection>
+                <View>
+                    <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+                </View>
+
+                <View style={styles.headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
+};
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+    state = { albums: [] };
 
     componentWillMount() {
-        console.log('componentWillMount');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />);
     }
 
     render() {
+        console.log(this.state.albums);
         return (
             <View>
-                <Text>Test</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
